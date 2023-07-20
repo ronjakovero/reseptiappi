5e2069afc6aa6db5d726e928eaa266fad2c88035
Ronja Kovero <ronja.kovero@gmail.com>
2023-07-20 11:13:52 +0300

    testi commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! Hi again, android, testi, testi2, testcommit</Text>
       <StatusBar style="auto" />
     </View>
   );
